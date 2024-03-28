e22c696c52f62ad013a8dc1a89525f023164eeb3
Yeonjoo Lee <joos0207@naver.com>
2024-03-28 23:35:01 +0900

    연습

--- JPQL/src/main/java/hellojpa/jpql/JpqlApplication.java
+++ JPQL/src/main/java/hellojpa/jpql/JpqlApplication.java
@@ -20,50 +20,38 @@ public class JpqlApplication {
 		tx.begin(); //트랜잭션 시작
 
 		try {
-			Team teamA = new Team();
-			teamA.setName("팀A");
-			em.persist(teamA);
+			Team team = new Team();
+			team.setName("팀A");
+			em.persist(team);
 
-			Team teamB = new Team();
-			teamB.setName("팀B");
-			em.persist(teamB);
+			Team team2 = new Team();
+			team2.setName("팀B");
+			em.persist(team2);
 
-			Member member1 = new Member();
-			member1.setUsername("회원1");
-			member1.setTeam(teamA);
-			em.persist(member1);
+			Member member = new Member();
+			member.setUsername("회원A");
+			member.setTeam(team);
+			member.setAge(20);
+			member.setType(MemberType.USER);
+			em.persist(member);
 
 			Member member2 = new Member();
-			member2.setUsername("회원2");
-			member2.setTeam(teamA);
+			member2.setUsername("회원B");
+			member2.setTeam(team2);
+			member2.setAge(20);
+			member2.setType(MemberType.USER);
 			em.persist(member2);
 
-			Member member3 = new Member();
-			member3.setUsername("회원3");
-			member3.setTeam(teamB);
-			em.persist(member3);
+			em.flush();
+			em.clear();
 
-			//em.flush();
-			//em.clear();
-
-			//FLUSH 자동 호출 -> flush는 commit하거나 query가 나갈 때 자동 호출된다. 혹은 강제 호출 -> 영속성 컨텍스트 걱정 안해도 된다. 근데 벌크 연산 쿼리가 나간 후엔 영속성 컨텍스트 초기화 해줘야 한다!
-			int resultCount = em.createQuery("update Member m set m.age = 20")
-							.executeUpdate(); //벌크 연산. 쿼리가 한번만 나간다.
-			em.clear(); //영속성 컨텍스트를 초기화해준 후, 다시 멤버를 조회해야 db의 내용이 반영된다!
-			Member findMember = em.find(Member.class, member1.getId());
-			System.out.println("findMember = "+ findMember.getAge());
-
-			System.out.println("resultCount = "+ resultCount);
-			System.out.println("member1.getAge() = "+ member1.getAge());  //db에만 반영되고, 영속성 컨텍스트엔 반영x
-			System.out.println("member2.getAge() = "+ member2.getAge());  //flush 시점의 데이터가 아직 영속성 컨텍스트에 있다.
-			System.out.println("member3.getAge() = "+ member3.getAge());
 
 
 			tx.commit(); //트랜잭션 끝 -> 저장(커밋)
 
 		} catch (Exception e) {
 			tx.rollback();
-			//e.printStackTrace();
+			e.printStackTrace();
 		}finally {
 			em.close(); //em과 emf를 다 사용 후엔 닫아준다.
 		}
